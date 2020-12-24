92a9e2237e343bbd609e53717dee7d41743368a4
tonymatheus <tony.chaves14@gmail.com>
2020-12-24 11:33:06 -0300

    Configura redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReacDom from 'react-dom';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
+
 import App from './App';
 
-ReacDom.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+ReacDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
